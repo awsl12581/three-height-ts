545e75fc64ed34a9110244fb3d2d3dfc969ddd41
nongChaTea <68147123+awsl12581@users.noreply.github.com>
2023-05-16 10:46:17 +0800

    更新注释

--- src/modules/voronoi/voronoi.ts
+++ src/modules/voronoi/voronoi.ts
@@ -24,20 +24,52 @@ class VoronoiMap {
    */
   points: Array<CoordinateValue2d>;
 
+  /**
+   * 随机点的数量
+   */
   numRegions: number;
 
+  /**
+   * 半边数量/3 = 三角形数量
+   */
   numTriangles: number;
 
+  /**
+   * 半边数量
+   */
   numEdges: number;
 
+  /**
+   * 半边数组，索引形式进行存储
+   * 对于一个半边e：(A----->B)
+   *
+   * triangles[e] = indexA 返回半边开始的Point索引 Point(idA)
+   *
+   * halfedges[e] = indexB 返回相邻三角形中的对边半边
+   *                       如果没有相邻三角形则返回-1 Point(idB)
+   */
   halfEdges: Int32Array;
 
+  /**
+   * 逆时针三个坐标索引组成一个三角形，形成整体数组
+   * 三角形 t 的半边是 3*t 、 3*t + 1 和 3*t + 2
+   * triangles[e] 返回半边开始的点id，因此三角形的三个点的坐标索引在triangles是紧邻的
+   */
   triangles: Uint32Array;
 
+  /**
+   * 计算出的基于三角剖分的三角形的外心的平面坐标位置
+   */
   centers: Array<CoordinateValue2d>;
 
+  /**
+   * 计算出的，关于维诺图胞体的集合
+   */
   cells!: VoronoiCells;
 
+  /**
+   * 计算出的，关于维诺图边的集合
+   */
   vertices!: VoronoiVertices;
 
   /**
@@ -100,42 +132,78 @@ class VoronoiMap {
    * @private
    */
   private createVoronoiCells() {
+    // 针对于triangles每一个点（一定会有重复的索引数）
+    // 每一个点表示一个半边的开始点，将其视为income
+    // 由于triangles[e] 返回半边开始的点id
+    // 对于每一个半边e
     for (let e = 0; e < this.triangles.length; e += 1) {
+      // 计算传入半边下一个半边的临边（临边，就是双向边的另一部分）
       const p = this.triangles[nextHalfedge(e)];
+      // 如果这个临边id在点的范围
       if (p < this.points.length && !this.cells.c[p]) {
+        // 计算传入临边所指向的点的所有入度边的集合
         const edges = edgesAroundPoint(this.halfEdges, e);
-        // cell: adjacent vertex
+        // 对每一个入度边，计算其所在的三角形，并形成集合
         this.cells.v[p] = edges.map((edgeIndex) => triangleOfEdge(edgeIndex));
+        // 对每一个入度边，计算其相邻的对边
         this.cells.c[p] = edges.map((edgeIndex) => this.triangles[edgeIndex])
           .filter((c: number) => c < this.points.length); // cell: adjacent valid cells
+        // 标定其是否为边界
         this.cells.b[p] = edges.length > this.cells.c[p].length ? 1 : 0; // cell: is border
       }
 
+      // 对于每一个半边e，计算其所在的三角形编号
       const t = triangleOfEdge(e);
       if (!this.vertices.p[t]) {
+        // 计算器三角形的心Point
         this.vertices.p[t] = this.triangleCenter(t); // vertex: coordinates
+        // 计算其周围三角形的id
         this.vertices.v[t] = this.trianglesAdjacentToTriangle(t); // vertex: adjacent vertices
+        // 计算三角形三个点的索引
         this.vertices.c[t] = this.pointsOfTriangle(t); // vertex: adjacent cells
       }
     }
   }
 
+  /**
+   * 给定一个三角形`t{A--[1]-->B--[2]-->C--[3]-->A}`
+   *
+   * 返回其外心左边Point
+   *
+   * @param t 三角形id
+   * @returns 对应三角形的外心
+   */
   triangleCenter(t: number) {
+    // 三条边的坐标集合数组
     const vertices = this.pointsOfTriangle(t)
       .map((p: number) => this.points[p]);
     return circumcenter(vertices[0], vertices[1], vertices[2]);
   }
 
+  /**
+   * @param t 三角形id
+   * @returns 返回三角形的三个点在points的索引
+   */
   pointsOfTriangle(t: number) {
+    // map() 方法返回一个新数组，数组中的元素为原始数组元素调用函数处理后的值。
     return edgesOfTriangle(t)
       .map((edge) => this.triangles[edge]);
   }
 
+  /**
+   * 给定三角形周围邻近的三角形id数组<=3
+   * @param t 给定的三角形id
+   * @returns 计算给定三角形周围邻近的三角形（紧邻三条边的三角形）
+   */
   trianglesAdjacentToTriangle(t: number) {
     const triangles = [];
-    for (let i = 0; i < edgesOfTriangle(t).length; i += 1) {
-      const edge = edgesOfTriangle(t)[i];
+    const edges = edgesOfTriangle(t);
+    // 针对三角形的每个边
+    for (let i = 0; i < edges.length; i += 1) {
+      const edge = edges[i];
+      // 计算其另一个半边id
       const opposite = this.halfEdges[edge];
+      // 计算对应半边所在的三角形
       triangles.push(triangleOfEdge(opposite));
     }
     return triangles;
--- src/modules/voronoi/voronoiUtils.ts
+++ src/modules/voronoi/voronoiUtils.ts
@@ -2,35 +2,46 @@ import { CoordinateValue2d } from '@/modules/voronoi/voronoiInterface';
 import Delaunator from 'delaunator';
 
 /**
- * 同一个三角形，计算下一个半边索引
- * @param e
+ * 同一个三角形，计算下一个半边索引  [A--1-->B--2-->C--3-->A]
+ * @param e 给定一条半边，他一定隶属于一个三角形（但不一定有其对边）
  */
 const nextHalfedge = (e: number) => (e % 3 === 2 ? e - 2 : e + 1);
 
 /**
- * 同一个三角形，计算上一个半边
+ * 同一个三角形，计算上一个半边索引
  * @param e
  */
 const prevHalfedge = (e: number) => (e % 3 === 0 ? e + 2 : e - 1);
 
 /**
- * 给定三角形的三个索引点
+ * 三角形的边
  * @param t 给定的三角形id
+ * @return [] 三角形对应的三条边id
  */
 const edgesOfTriangle = (t: number) => [3 * t, 3 * t + 1, 3 * t + 2];
 
 /**
- * 给定一个半边id，其所属的三角形
- * @param e
+ * 边对应的三角形
+ * @param e 给定一条边id
+ * @return t:number 边所在的三角形
  */
 const triangleOfEdge = (e: number) => Math.floor(e / 3);
 
+/**
+ * 构建多边形所使用
+ * @param halfedges 半边数组
+ * @param start 一条半边
+ * @returns 一个点的所有入度的边
+ */
 const edgesAroundPoint = (halfedges: Int32Array, start: number) => {
   const result = [];
   let incoming = start;
   do {
+    // 对于一个半边，将其存储至结果集
     result.push(incoming);
+    // 计算其下一个半边
     const outgoing = nextHalfedge(incoming);
+    // 下一个半边的对边
     incoming = halfedges[outgoing];
   } while (incoming !== -1 && incoming !== start && result.length < 20);
   return result;
